c1f47015d33f6cf452fd5e2b69344d7f94285e37
Muhamad Faisal <muhamadfaisal280201@gmail.com>
2022-07-22 23:08:34 +0700

    Update Repository

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sevendays/pages/first_empty.dart';
 import 'package:sevendays/pages/first_sign.dart';
 import 'package:sevendays/pages/second_sign.dart';
 
@@ -11,7 +12,7 @@ class SevenDays extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SecondSign(),
+      home: FirstEmpty(),
     );
   }
 }
---
+++ lib/widgets/theme.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// Empty State v1
+Color darkBlue = const Color(0xff0E1954);
+
+TextStyle semiBoldTextStyle = GoogleFonts.poppins(
+  color: darkBlue,
+  fontSize: 24,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle lightTextStyle = GoogleFonts.poppins(
+  color: darkBlue,
+  fontSize: 16,
+);
+
+TextStyle buttonTextStyle = GoogleFonts.openSans(
+  color: const Color(0xffF8F8F8),
+  fontSize: 24,
+  fontWeight: FontWeight.w600,
+);
+
+// Empty State v2
+Color white = const Color(0xffFFFFFF);
+
+TextStyle titleTextStyle = GoogleFonts.poppins(
+  color: white,
+  fontSize: 24,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle desTextStyle = GoogleFonts.poppins(
+  color: white,
+  fontSize: 16,
+  fontWeight: FontWeight.w300,
+);
